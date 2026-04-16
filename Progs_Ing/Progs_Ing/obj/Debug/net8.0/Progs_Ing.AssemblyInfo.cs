b156a766f3f1f47d74d74d42cdb5a2ab0ebe0c39
miguelel0y <migueleloy2004@gmail.com>
2026-04-16 14:35:57 -0600

    upp

--- Progs_Ing/Progs_Ing/obj/Debug/net8.0/Progs_Ing.AssemblyInfo.cs
+++ Progs_Ing/Progs_Ing/obj/Debug/net8.0/Progs_Ing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Progs_Ing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab467ef04e67202d2c74ffab84d866872fc9934")]
 [assembly: System.Reflection.AssemblyProductAttribute("Progs_Ing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Progs_Ing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
